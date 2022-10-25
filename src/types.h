64ec2bf81c4860288f8f3a47c6b1d886b84ed8a0
Timon Fercho <tfercho@student.ethz.ch>
2022-10-26 00:14:27 +0200

    fix(types): properly scope type definitions

--- src/types.h
+++ src/types.h
@@ -1,11 +1,14 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
-typedef signed long int64_t;
-typedef unsigned long size_t;
-typedef unsigned char uint8_t;
-typedef int64_t list_id_t;
-typedef float vector_el_t;
-typedef int64_t vector_id_t;
+namespace ann_dkvs
+{
+  typedef signed long int64_t;
+  typedef unsigned long size_t;
+  typedef unsigned char uint8_t;
+  typedef int64_t list_id_t;
+  typedef float vector_el_t;
+  typedef int64_t vector_id_t;
+}
 
 #endif // TYPES_H_
