a4940c9791fb405e30e81a883590ff2802b957fa
Timon Fercho <tfercho@student.ethz.ch>
2022-11-09 15:05:00 +0100

    test(inverted-lists): add tests for update_entries()

--- tests/InvertedListsTest.cpp
+++ tests/InvertedListsTest.cpp
@@ -35,7 +35,7 @@ auto get_total_size = [](InvertedLists lists, size_t used_space)
   return total_size;
 };
 
-SCENARIO("InvertedLists can be constructed", "[InvertedLists]")
+SCENARIO("InvertedLists(): an InvertedLists object can be constructed", "[InvertedLists]")
 {
   GIVEN("a vector dimension and a filename")
   {
@@ -74,7 +74,7 @@ SCENARIO("InvertedLists can be constructed", "[InvertedLists]")
   }
 }
 
-SCENARIO("inverted lists can be created", "[InvertedLists]")
+SCENARIO("create_list(): inverted lists can be created", "[InvertedLists]")
 {
 
   GIVEN("an InvertedLists object")
@@ -193,12 +193,15 @@ SCENARIO("inverted lists can be created", "[InvertedLists]")
       list_id_t list_id = 1;
       len_t list_length = 0;
 
-      REQUIRE_THROWS_WITH(lists.create_list(list_id, list_length), Contains("greater than 0"));
+      THEN("an exception is thrown")
+      {
+        REQUIRE_THROWS(lists.create_list(list_id, list_length));
+      }
     }
   }
 }
 
-SCENARIO("the free space of inverted lists can be queried", "[InvertedLists]")
+SCENARIO("get_free_space(): the free space of an InvertedLists object is as expected", "[InvertedLists]")
 {
 
   GIVEN("an InvertedLists object storing vectors of dimension 1")
@@ -270,6 +273,145 @@ SCENARIO("the free space of inverted lists can be queried", "[InvertedLists]")
       THEN("the free space is 32 - list size")
       {
         REQUIRE(lists.get_free_space() == 32 - list_size);
+      }
+    }
+  }
+}
+
+SCENARIO("update_entries(): multiple entries of a list can be updated", "[InvertedLists]")
+{
+  GIVEN("an InvertedLists object and two lists of 1D vectors and corresponding ids")
+  {
+    size_t vector_dim = 1;
+    string filename = "lists.bin";
+    InvertedLists lists = InvertedLists(vector_dim, filename);
+    vector_el_t vectors[5] = {6.0, 7.0, 8.0, 9.0, 10.0};
+    list_id_t ids[5] = {1, 2, 3, 4, 5};
+    vector_el_t vectors2[5] = {11.0, 12.0, 13.0, 14.0, 15.0};
+    list_id_t ids2[5] = {6, 7, 8, 9, 10};
+
+    WHEN("an inverted list is created")
+    {
+      list_id_t list_id = 1;
+      len_t list_length = 5;
+      lists.create_list(list_id, list_length);
+      size_t total_size_before_update = lists.get_total_size();
+
+      AND_WHEN("all entries of the list are updated with the vectors and ids of the first list")
+      {
+        lists.update_entries(list_id, vectors, ids, 0, list_length);
+
+        THEN("all vectors are updated")
+        {
+          vector_el_t *list_vectors = lists.get_vectors(list_id);
+          for (len_t i = 0; i < list_length; i++)
+          {
+            REQUIRE(list_vectors[i] == vectors[i]);
+          }
+        }
+
+        THEN("all ids are updated")
+        {
+          list_id_t *list_ids = lists.get_ids(list_id);
+          for (len_t i = 0; i < list_length; i++)
+          {
+            REQUIRE(list_ids[i] == ids[i]);
+          }
+        }
+
+        THEN("the list length is unaffected")
+        {
+          REQUIRE(lists.get_list_length(list_id) == list_length);
+        }
+
+        THEN("the total size is unaffected")
+        {
+          REQUIRE(lists.get_total_size() == total_size_before_update);
+        }
+
+        AND_WHEN("only the first few entries are updated with the second list")
+        {
+          len_t update_length = 3;
+          lists.update_entries(list_id, vectors2, ids2, 0, update_length);
+
+          THEN("only the first few entries are updated with the vectors from the second list")
+          {
+            vector_el_t *list_vectors = lists.get_vectors(list_id);
+            for (len_t i = 0; i < update_length; i++)
+            {
+              REQUIRE(list_vectors[i] == vectors2[i]);
+            }
+            for (len_t i = update_length; i < list_length; i++)
+            {
+              REQUIRE(list_vectors[i] == vectors[i]);
+            }
+          }
+
+          THEN("only the first few entries are updated with the ids from the second list")
+          {
+            list_id_t *list_ids = lists.get_ids(list_id);
+            for (len_t i = 0; i < update_length; i++)
+            {
+              REQUIRE(list_ids[i] == ids2[i]);
+            }
+            for (len_t i = update_length; i < list_length; i++)
+            {
+              REQUIRE(list_ids[i] == ids[i]);
+            }
+          }
+
+          THEN("the list length is unaffected")
+          {
+            REQUIRE(lists.get_list_length(list_id) == list_length);
+          }
+
+          THEN("the total size is unaffected")
+          {
+            REQUIRE(lists.get_total_size() == total_size_before_update);
+          }
+        }
+
+        AND_WHEN("only the last few entries are updated with the second list")
+        {
+          len_t update_length = 3;
+          lists.update_entries(list_id, vectors2, ids2, list_length - update_length, update_length);
+
+          THEN("only the last few entries are updated with the vectors from the second list")
+          {
+            vector_el_t *list_vectors = lists.get_vectors(list_id);
+            for (len_t i = 0; i < list_length - update_length; i++)
+            {
+              REQUIRE(list_vectors[i] == vectors[i]);
+            }
+            for (len_t i = list_length - update_length; i < list_length; i++)
+            {
+              REQUIRE(list_vectors[i] == vectors2[i - (list_length - update_length)]);
+            }
+          }
+
+          THEN("only the last few entries are updated with the ids from the second list")
+          {
+            list_id_t *list_ids = lists.get_ids(list_id);
+            for (len_t i = 0; i < list_length - update_length; i++)
+            {
+              REQUIRE(list_ids[i] == ids[i]);
+            }
+            for (len_t i = list_length - update_length; i < list_length; i++)
+            {
+              REQUIRE(list_ids[i] == ids2[i - (list_length - update_length)]);
+            }
+          }
+
+          THEN("the list length is unaffected")
+          {
+            REQUIRE(lists.get_list_length(list_id) == list_length);
+          }
+
+          THEN("the total size is unaffected")
+          {
+            REQUIRE(lists.get_total_size() == total_size_before_update);
+          }
+        }
       }
     }
   }
