881f7cd38039a5bd38a3420916cf09210f18ffae
Timon Fercho <tfercho@student.ethz.ch>
2023-01-02 15:18:29 +0100

    ref(types): reorder types for clarity

--- include/storage-node/types.hpp
+++ include/storage-node/types.hpp
@@ -3,12 +3,15 @@
 
 namespace ann_dkvs
 {
-  typedef signed long int64_t;
+  // 1 byte
+  typedef unsigned char uint8_t;
+  // 4 bytes
+  typedef float vector_el_t;
+  // 8 bytes
   typedef unsigned long len_t;
   typedef unsigned long size_t;
-  typedef unsigned char uint8_t;
+  typedef signed long int64_t;
   typedef int64_t list_id_t;
-  typedef float vector_el_t;
   typedef int64_t vector_id_t;
 }
 
