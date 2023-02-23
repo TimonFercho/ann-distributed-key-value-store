4af5ed400397e029b9cf7d17209dd5023ce94606
Timon Fercho <tfercho@student.ethz.ch>
2023-02-23 23:48:53 +0100

    fix(main): avoid unused params compiler warning

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <iomanip>
 
-#ifdef _OPENMP
-#include <omp.h>
-#endif
+#define UNUSED(x) (void)(x)
 
 #include "StorageLists.hpp"
 
@@ -11,21 +9,7 @@ using namespace ann_dkvs;
 
 int main(int argc, char const *argv[])
 {
-#ifdef _OPENMP
-	std::cout << "OpenMP is enabled" << std::endl;
-#else
-	std::cout << "OpenMP is disabled" << std::endl;
-#endif
-#ifdef _OPENMP
-	omp_set_num_threads(256);
-#pragma omp parallel
-	{
-		int id = omp_get_thread_num();
-#pragma omp critical
-		{
-			std::cout << "Hello World from thread " << std::setfill('0') << std::setw(2) << std::right << id << "\n";
-		}
-	}
-#endif
+	UNUSED(argc);
+	UNUSED(argv);
 	return 0;
 }
