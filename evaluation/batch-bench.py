6421443cb6eb0f358763a0576b6b6b5f2fc0e376
Timon Fercho <tfercho@student.ethz.ch>
2023-02-28 01:36:37 +0100

    feat(evaluation): abbreviate filenames + introduce subdirs for batch-bench

--- evaluation/batch-bench.py
+++ evaluation/batch-bench.py
@@ -6,7 +6,7 @@ import sys
 
 
 def read_benchmarks():
-    with open('benchmarks.json') as f:
+    with open(os.path.join('evaluation', 'benchmarks.json')) as f:
         benchmarks = json.load(f)
     return benchmarks
 
@@ -37,28 +37,45 @@ def get_benchmark_command(params):
 
 
 def get_filename(params):
-    EXCLUDE_KEYS = [
-        'DEBUG', 'run', 'make_command', 'executable', 'reporter', 'catch_tags'
-    ]
-    directory = 'evaluation/benchmarks'
+    EXCLUDE_KEYS = ['DEBUG']
+    directory = os.path.join('evaluation', 'benchmarks')
+    if params['subdir']:
+        directory = os.path.join(directory, params['subdir'])
+        os.makedirs(directory, exist_ok=True)
     filename = f'{params["catch_tags"]} '
     filename += ' '.join([
         f"{key}={value}" for key, value in sorted(params.items())
-        if key not in EXCLUDE_KEYS
+        if (key not in EXCLUDE_KEYS and key[0].isupper()) or key[:4] == 'env_'
     ])
-    filename += ".xml"
+    filename += f'.{params["reporter"]}'
+    ABBREVIATIONS = {
+        "MAX_BUFFER_SIZE": "BUF",
+        "DYNAMIC_INSERTION": "DIN",
+        "MIN_N_ENTRIES_PER_LIST": "MEN",
+        "MIN_TOTAL_SIZE_BYTES": "MSI",
+        "TEST_N_LISTS": "NLI",
+        "TEST_N_PROBES": "NPR",
+        "TEST_N_RESULTS": "NRE",
+        "TEST_N_DIMS": "NDI",
+        "env_OMP_NUM_THREADS": "NTH",
+        "PMODE": "PMO",
+        "USE_SIMD": "SIM",
+        "USE_OMP": "OMP"
+    }
+    for key, value in ABBREVIATIONS.items():
+        filename = filename.replace(key, value)
+
     return os.path.join(directory, filename)
 
-
-benchmarks = read_benchmarks()
 
 if os.path.basename(os.getcwd()) == 'evaluation':
     os.chdir('..')
 
+benchmarks = read_benchmarks()
+
 default_params = benchmarks['defaults']
 benchmarks['benchmarks'] = [b for b in benchmarks['benchmarks'] if b['run']]
 
-# read first argument
 dry_run = False
 if len(sys.argv) > 1:
     dry_run = sys.argv[1] == 'dry'
@@ -66,21 +83,25 @@ if len(sys.argv) > 1:
 for benchmark in tqdm(benchmarks['benchmarks']):
     params = default_params.copy()
     params.update(benchmark)
-    print("=========================================")
     filename = get_filename(params)
-    print(filename)
-    make_cmd = get_make_command(params)
     if dry_run:
-        print()
-        print(make_cmd)
-    else:
+        print('=====================')
+    print(filename)
+
+    print()
+    make_cmd = get_make_command(params)
+    print(make_cmd)
+    if not dry_run:
         os.system(make_cmd)
 
+    if not dry_run and not os.path.exists(params['executable']):
+        print(f"Executable {params['executable']} does not exist.")
+        exit(1)
+
+    print()
     envs = get_environment_variables(params)
     benchmark_cmd = get_benchmark_command(params)
     envs_benchmark_cmd = f'export{envs}; {benchmark_cmd} >> "{filename}"'
-    if dry_run:
-        print()
-        print(envs_benchmark_cmd)
-    else:
+    print(envs_benchmark_cmd)
+    if not dry_run:
         os.system(envs_benchmark_cmd)
